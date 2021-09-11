e3548e3f4c8cb38408c803760446edc44fd98af1
Dev <DevDeCoste@users.noreply.github.com>
2021-09-11 11:08:35 -0400

    Built Mortgage Calc Command Line App

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -17,6 +17,15 @@ public class MortgageCalculator {
 
     public static void main(String[] args) {
 
+        long loanAmount = Long.parseLong(args[0]);
+        int termInYears = Integer.parseInt(args[1]);
+        float annualRate = Float.parseFloat(args[2]);
+
+        MortgageCalculator calculator = new MortgageCalculator();
+        calculator.calculateMonthlyPayment();
+        System.out.println(calculator.toString());
+
+
     } // Main method
 
 
