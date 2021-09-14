00b7ad32682ef67daea3e7cecf7e0eef11d1b0e1
Dev DeCoste <DevDeCoste@users.noreply.github.com>
2021-09-14 13:47:50 -0400

    commandsToUsage Map initialized

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -1,7 +1,17 @@
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
 public class Finance {
 
     public final static String BEST_LOAN_RATES = "bestLoanRates";
     public final static String SAVINGS_CALCULATOR = "savingsCalculator";
     public final static String MORTGAGE_CALCULATOR = "mortgageCalculator";
+
+    public final static Map<String,String> commandsToUsage = Map.of(BEST_LOAN_RATES, "usage: bestLoanRates",
+            SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
+            MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>");
+
+
 
 } // End Finance Class
