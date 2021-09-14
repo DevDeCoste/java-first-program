9df40d995adc74c89cb870ef79f77245be530288
Dev <DevDeCoste@users.noreply.github.com>
2021-09-14 17:55:10 -0400

    Called String, int and float methods

--- src/main/java/Utilities.java
+++ src/main/java/Utilities.java
@@ -1,4 +1,6 @@
 public class Utilities {
+
+
     public static long getLongValue(String in) {
         long out = Long.MIN_VALUE;
         try{
@@ -6,6 +8,27 @@ public class Utilities {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(in + " cannot be converted into a 'long' value. Exiting program.");
         }
+        return out;
+    } // End getLongValue Method
+
+    public static int getIntValue(String in) {
+        int out = Integer.MIN_VALUE;
+
+        try {
+            Integer.parseInt(in);
+        } catch(NumberFormatException e) {
+            throw new IllegalArgumentException(in + " cannot be converted into a 'int' value. Exiting program.");
+        }
+        return out;
+    }
+
+    public static float getFlotValue(String in) {
+        float out = Float.MIN_VALUE;
+                try{
+                    Float.parseFloat(in);
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException(in + " cannot be converted into a 'float' value. Exiting program.");
+                }
         return out;
     }
 
